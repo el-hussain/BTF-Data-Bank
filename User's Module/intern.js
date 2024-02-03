1c27784f497d1438057491c0f68837217dded58c
idrisshehehu96 <idrisshehu96@gmail.com>
2024-02-03 22:36:29 +0100

    I added picture input to both member and intern forms

--- User's Module/intern.js
+++ User's Module/intern.js
@@ -1,25 +1,31 @@
-    const studentRadio = document.getElementById('student');
+//picture upload and previev
+function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function(){
+      var output = document.getElementById('output');
+      output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+
+// Toggling of NYSC and School information for student and corp member interns
+const studentRadio = document.getElementById('student');
     const corpMemberRadio = document.getElementById('corpMember');
     const studentForm = document.getElementById('studentIntern');
     const corpMemberForm = document.getElementById('nyscIntern');
 
-    studentRadio.addEventListener('click', function(){
-      if (studentRadio.checked) {
-          studentForm.classList.remove('hidden');
-          corpMemberForm.classList.add('hidden');
-      } else if (corpMemberRadio.checked) {
-          studentForm.classList.add('hidden');
-          corpMemberForm.classList.remove('hidden');
-      }
-  }
-    );
-    corpMemberRadio.addEventListener('click', function(){
-      if (studentRadio.checked) {
-          studentForm.classList.remove('hidden');
-          corpMemberForm.classList.add('hidden');
-      } else if (corpMemberRadio.checked) {
-          studentForm.classList.add('hidden');
-          corpMemberForm.classList.remove('hidden');
-      }
-  }
-    );
+    studentRadio.addEventListener('click', internCategorySelection);
+    corpMemberRadio.addEventListener('click', internCategorySelection);
+
+    function internCategorySelection(){
+        if (studentRadio.checked) {
+            studentForm.classList.remove('hidden');
+            corpMemberForm.classList.add('hidden');
+        } else if (corpMemberRadio.checked) {
+            studentForm.classList.add('hidden');
+            corpMemberForm.classList.remove('hidden');
+     }
+    }
+
+    
